503288372d3dd35955cfd177b56ee23b3bafbe4b
Liu Jinlong <q641480816@gmail.com>
2018-10-25 01:13:17 +0800

    deploy web

--- VAWeb/vaweb/build/precache-manifest.06b345ccdc8e8f03b0eb7aa87f7a3af3.js
+++ VAWeb/vaweb/build/precache-manifest.06b345ccdc8e8f03b0eb7aa87f7a3af3.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "4de03e54daf40ff74d98",
-    "url": "/static/js/main.4de03e54.chunk.js"
+    "revision": "bcb5ccce3e540c00498c",
+    "url": "/static/js/main.bcb5ccce.chunk.js"
   },
   {
     "revision": "196a5a6d8aa2eafb0bbf",
     "url": "/static/js/1.196a5a6d.chunk.js"
   },
   {
-    "revision": "4de03e54daf40ff74d98",
+    "revision": "bcb5ccce3e540c00498c",
     "url": "/static/css/main.6c1c0b8e.chunk.css"
   },
   {
-    "revision": "b2b2cc22e4b41b2d5d4741925d212693",
+    "revision": "53f4afe41cd33f5ca11c2cff655c5c26",
     "url": "/index.html"
   }
 ];
